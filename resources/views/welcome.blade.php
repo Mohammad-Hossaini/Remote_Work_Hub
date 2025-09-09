0278e785eb10e6226d9aad8a119dfe3a125e7df5
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-09-09 00:56:10 -0700

    Connected React 18 and Laravel 10

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Remote Work Hub</title>
+</head>
+<body>
+    <id id = 'app'></id>
+@viteReactRefresh
+@vite('resources/js/app.js')
+</body> 
+</html>
