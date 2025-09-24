31f575428320b7e4e2a368da30dc3101e5bb5966
Nisar Ahmad Ramzi <nisarahmadramzi360@gmail.com>
2025-09-24 20:37:08 +0430

    backend the companySeeder completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProfileSeeder::class,
+            CompanySeeder::class,
             // JobSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
